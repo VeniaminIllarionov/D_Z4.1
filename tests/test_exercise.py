ea6d9583a77406d2071f61b31c5827ae0b545af5
VeniaminIllarionov <venya957@gmail.com>
2024-02-13 01:58:02 +0300

    Initial commit

---
+++ tests/test_exercise.py
@@ -0,0 +1,66 @@
+import pytest
+from utils.exercise_1 import Category, Product
+
+
+@pytest.fixture
+def category():
+    return Category(["Смартфоны"], "Смартфон облегчают жизнь", [
+        {
+            "name": "Samsung Galaxy C23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5
+        },
+        {
+            "name": "Iphone 15",
+            "description": "512GB, Gray space",
+            "price": 210000.0,
+            "quantity": 8
+        },
+        {
+            "name": "Xiaomi Redmi Note 11",
+            "description": "1024GB, Синий",
+            "price": 31000.0,
+            "quantity": 14
+        }])
+
+
+def test_init(category):
+    assert category.name == ["Смартфоны"]
+    assert category.description == "Смартфон облегчают жизнь"
+    assert category.products == [
+        {
+            "name": "Samsung Galaxy C23 Ultra",
+            "description": "256GB, Серый цвет, 200MP камера",
+            "price": 180000.0,
+            "quantity": 5
+        },
+        {
+            "name": "Iphone 15",
+            "description": "512GB, Gray space",
+            "price": 210000.0,
+            "quantity": 8
+        },
+        {
+            "name": "Xiaomi Redmi Note 11",
+            "description": "1024GB, Синий",
+            "price": 31000.0,
+            "quantity": 14
+        }]
+    assert category.total_quantity_category == 1
+    sum = 0
+    for i in category.products:
+        sum += i["quantity"]
+    assert sum == 27
+
+
+@pytest.fixture
+def product():
+    return Product("Iphone15", "512GB, Gray space", 31_000.0, 8)
+
+
+def test_init_1(product):
+    assert product.name == "Iphone15"
+    assert product.description == "512GB, Gray space"
+    assert product.price == 31000.0
+    assert product.quantity == 8
---
+++ utils/exercise_1.py
@@ -0,0 +1,27 @@
+class Category:
+    '''класс Category с атрибутами имя, описание, товары, общее количество категорий и общее количество уникальных продуктов'''
+    name: list
+    description: str
+    products: list
+
+    def __init__(self, name, description, products):
+        global sum
+        self.name = name
+        self.description = description
+        self.products = products
+        self.total_quantity_category = len(self.name)
+        self.total_quantity_product = [elem["quantity"] for elem in products]
+
+
+class Product:
+    '''класс Product с атрибутами имя, описание, ценой, количества'''
+    name: str
+    description: list
+    price: float
+    quantity: int
+
+    def __init__(self, name, description, price, quantity):
+        self.name = name
+        self.description = description
+        self.price = price
+        self.quantity = quantity
