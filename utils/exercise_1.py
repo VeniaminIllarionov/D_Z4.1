e1d991591e925f5a475021782f6e704e1d9eae2d
VeniaminIllarionov <venya957@gmail.com>
2024-02-13 09:35:03 +0300

    'fix'

--- utils/exercise_1.py
+++ utils/exercise_1.py
@@ -1,3 +1,6 @@
+import json
+
+
 class Category:
     '''класс Category с атрибутами имя, описание, товары, общее количество категорий и общее количество уникальных продуктов'''
     name: list
@@ -25,3 +28,21 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+
+def load_file():
+    with open('product.json', "r", encoding="utf-8") as file:
+        return json.load(file)
+
+
+produc = load_file()
+
+for i in produc:
+    category_ = Category(i["name"], i["description"], i["products"])
+    for elem in i["products"]:
+        product = Product(elem['name'], elem['description'], elem['price'], elem['quantity'])
+
+
+print(f"{category_.name}, {category_.description}, {category_.products}")
+print(f"{product.name}, {product.description}, {product.price}, {product.quantity}")
+
