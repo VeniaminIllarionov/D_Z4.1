8263049385b3517eb45b45967c994100c5a72046
VeniaminIllarionov <venya957@gmail.com>
2024-02-15 22:42:48 +0300

    ''

--- tests/test_exercise.py
+++ tests/test_exercise.py
@@ -53,6 +53,20 @@ def test_init(category):
         sum += i["quantity"]
     assert sum == 27
     assert len(category.products) == 3
+    assert category.append_product({
+        "name": "Samsung A51",
+        "description": "1024GB, Белый",
+        "price": 51000.0,
+        "quantity": 14}) == [{'name': 'Samsung Galaxy C23 Ultra', 'description': '256GB, Серый цвет, 200MP камера',
+                              'price': 180000.0, 'quantity': 5},
+                             {'name': 'Iphone 15', 'description': '512GB, Gray space',
+                              'price': 210000.0, 'quantity': 8},
+                             {'name': 'Xiaomi Redmi Note 11', 'description': '1024GB, Синий', 'price': 31000.0,
+                              'quantity': 14},
+                             {'name': 'Samsung A51', 'description': '1024GB, Белый', 'price': 51000.0, 'quantity': 14}]
+    assert category.products_dispaly == "Samsung Galaxy C23 Ultra, 180000.0 руб. Остаток: 5 шт."
+
+
 
 
 @pytest.fixture
--- utils/exercise_1.py
+++ utils/exercise_1.py
@@ -1,6 +1,3 @@
-import json
-
-
 class Category:
     '''класс Category с атрибутами имя, описание, товары, общее количество категорий и общее количество уникальных
     продуктов'''
@@ -19,11 +16,17 @@ class Category:
     def products(self):
         return self.__products
 
-    @products.setter
-    def products(self, new_product):
+    def append_product(self, new_product=dict):
         self.__products.append(new_product)
+        return self.__products
 
-prod =
+    @property
+    def products_dispaly(self):
+        products = self.__products
+        for product in products:
+            return f"{product['name']}, {product['price']} руб. Остаток: {product['quantity']} шт."
+
+
 class Product:
     '''класс Product с атрибутами имя, описание, ценой, количества'''
     name: str
@@ -36,3 +39,26 @@ class Product:
         self.description = description
         self.price = price
         self.quantity = quantity
+
+    @staticmethod
+    def add_product():
+        product_dict = {"name": None, "description": None,
+                        "price": None, "quantity": None}
+        name_input = input("Введите наименование товара")
+        description_input = input("Введите описание товара")
+        price_input = input("Введите цену товара")
+        quantity_input = input("Введите количество товара")
+        product_dict["name"] = name_input
+        product_dict["description"] = description_input
+        product_dict["price"] = float(price_input)
+        product_dict["quantity"] = int(quantity_input)
+        return product_dict
+
+    # @classmethod
+    # def append_product(cls, new_product):
+    # for i in cls.products:
+    # if new_product['name'] in i['name']:
+    # i['quantity'] += new_product['quantity']
+    # elif new_product['name'] not in i['name']:
+
+    # cls.products.append(new_product)
